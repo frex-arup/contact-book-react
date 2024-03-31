b7be479e7754f319418159755207dc052dd89f09
nigam <nigammishra826@gmail.com>
2024-03-31 16:22:22 +0530

    some changes

--- src/pages/ViewAll.js
+++ src/pages/ViewAll.js
@@ -9,7 +9,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
@@ -18,7 +18,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a short card.</p>
       </div>
@@ -27,7 +27,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
@@ -36,7 +36,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
@@ -45,7 +45,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
@@ -54,7 +54,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
@@ -63,7 +63,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
@@ -72,7 +72,7 @@ const ViewAll = () => {
   <div className="col">
     <div className="card h-100">
       <img src={img} className="card-img-top imgdesign" alt="..." />
-      <div className="card-body">
+      <div className="card-body imgdesign">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
