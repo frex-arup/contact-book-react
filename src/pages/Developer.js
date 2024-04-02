8b9639277d9d6494ffb82b577d6884c8d840acec
nigam <nigammishra826@gmail.com>
2024-04-02 22:59:14 +0530

    developer.js

--- src/pages/Developer.js
+++ src/pages/Developer.js
@@ -36,6 +36,54 @@ const Developer = () => {
   </div>
   </div>
 </>
+
+
+<>
+<div className="container mt-5">
+      <div className="row">
+          <div className="col-md-6">
+              <img src={Nigam} alt="Developer" className="developer-img" />
+          </div>
+          <div className="col-md-6">
+              <h1>About the Developer</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt.
+                  Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Quisque velit tortor,
+                  commodo nec blandit vel, cursus nec elit. Cras ultricies ligula sed magna dictum porta.
+                  Pellentesque in ipsum id orci porta dapibus.</p>
+              <p>Social Media:</p>
+              <ul className="list-inline">
+                  <li className="list-inline-item"><a href="#"><img src={Facebook} alt="Facebook" width="40" /></a></li>
+                  <li className="list-inline-item"><a href="#"><img src={Instagram} alt="Instagram" width="30" /></a></li>
+                  <li className="list-inline-item"><a href="#"><img src={GitHub} alt="GitHub" width="50" /></a></li>
+                  <li className="list-inline-item"><a href="#"><img src={LinkedIn} alt="LinkedIn" width="40" /></a></li>
+              </ul>
+          </div>
+  </div>
+  </div>
+</>
+<>
+<div className="container mt-5">
+      <div className="row">
+          <div className="col-md-6">
+              <img src={Nigam} alt="Developer" className="developer-img" />
+          </div>
+          <div className="col-md-6">
+              <h1>About the Developer</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt.
+                  Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Quisque velit tortor,
+                  commodo nec blandit vel, cursus nec elit. Cras ultricies ligula sed magna dictum porta.
+                  Pellentesque in ipsum id orci porta dapibus.</p>
+              <p>Social Media:</p>
+              <ul className="list-inline">
+                  <li className="list-inline-item"><a href="#"><img src={Facebook} alt="Facebook" width="40" /></a></li>
+                  <li className="list-inline-item"><a href="#"><img src={Instagram} alt="Instagram" width="30" /></a></li>
+                  <li className="list-inline-item"><a href="#"><img src={GitHub} alt="GitHub" width="50" /></a></li>
+                  <li className="list-inline-item"><a href="#"><img src={LinkedIn} alt="LinkedIn" width="40" /></a></li>
+              </ul>
+          </div>
+  </div>
+  </div>
+</>
     </div>
   )
 }
